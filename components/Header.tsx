4560fdf71344c879061aee2a62af2fe9e974061d
Genzou <spookyjoness@outlook.com>
2022-01-24 16:20:36 +0800

    added channel and toolbar content with overlapping

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,37 @@
 import styles from '../styles/Header.module.css'
 import { BsMegaphoneFill } from "react-icons/bs"
 import { FaHashtag } from "react-icons/fa"
+import { SiGooglemessages } from "react-icons/Si"
+import { FaBell } from "react-icons/fa"
+import { BsPinAngleFill } from "react-icons/bs"
+import { ImUsers } from "react-icons/im"
+import { MdInbox } from "react-icons/md"
+import { IoMdHelpCircle } from "react-icons/io"
+// ImUsers
+// IoHelpCircle
 
 
 export default function Header() {
    return (
       <div className={styles.wrapper}>
-         <div className={styles["content-container"]}>
-            {/* <BsMegaphoneFill className={`${styles["channel-icon"]} ${styles["center-vertically"]}`} /> */}
-            <FaHashtag className={`${styles["channel-icon"]} ${styles["center-vertically"]}`} />
-            <h1 className={styles["center-vertically"]}>channel-name</h1>
-            <button className={`${styles["follow-button"]} ${styles["center-vertically"]}`}>Follow</button>
+         <div className={styles.test}>
+         <div className={styles["channel-container-shadow"]}>
+            <div className={styles["channel-container"]}>
+               {/* <BsMegaphoneFill className={`${styles["channel-icon"]} ${styles["center-vertically"]}`} /> */}
+               <FaHashtag className={styles["channel-icon"]} />
+               <h1>channel-name</h1>
+               <button className={styles["follow-button"]}>Follow</button>
+            </div>
+         </div>
+         <div className={styles["toolbar-container"]}>          
+            <SiGooglemessages className={styles["channel-icon"]}/>
+            <FaBell className={styles["channel-icon"]}/>
+            <BsPinAngleFill className={styles["channel-icon"]}/>
+            <ImUsers className={styles["channel-icon"]}/>
+            <input></input>
+            <MdInbox className={styles["channel-icon"]}/>
+            <IoMdHelpCircle className={styles["channel-icon"]}/>           
+         </div>
          </div>
       </div>
    )

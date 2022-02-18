8d48752c293344dac464f4ad07ee16acebaae71b
Genzou <spookyjoness@outlook.com>
2022-02-18 18:03:38 +0800

    improved server bar and icons

--- components/ServerBar.tsx
+++ components/ServerBar.tsx
@@ -1,33 +1,58 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Store } from '../state/reducers';
-import { setSelectedServer } from '../state/slices/selectionSlice';
-import { addServer } from '../state/slices/serversSlice';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Store } from "../state/reducers";
+import { setSelectedServer } from "../state/slices/selectionSlice";
+import { addServer } from "../state/slices/serversSlice";
+import { AiOutlinePlus } from "react-icons/ai";
+import { IoMdCompass } from "react-icons/io";
+import { FiDownload } from "react-icons/fi";
 
-import styles from '../styles/ServerBar.module.css'
-import ServerIcon from './ServerIcon';
+import styles from "../styles/ServerBar.module.css";
+import ServerIcon, { ServerIconColourVariant } from "./ServerIcon";
+import { FaDiscord } from "react-icons/fa";
 
 export default function ServerBar() {
-
    const dispatch = useDispatch();
    const servers = useSelector((state: Store) => state.servers);
 
    const [prevIndex, setPrevIndex] = useState<number>();
 
+   const placeholderImages = [
+      "https://121clicks.com/wp-content/uploads/2012/01/landscape_photographers_thumb.jpg",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flh3.googleusercontent.com%2FlKcnnutMn6ZP2dB5AFuufefh2Z2etbIdc4AL-t0YAW-aQAaBz-L_Xh9ZB9cBhUz-DLM%3Ds180&f=1&nofb=1",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.d1kfj3y4opCoATkdIeYr9QAAAA%26pid%3DApi&f=1",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.P_VYzSnceY5KL6qxqqqWQAAAAA%26pid%3DApi&f=1",
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.gluF7x_xq1bDseYcdgrVNAAAAA%26pid%3DApi&f=1",
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fcaptainkimo.com%2Fwp-content%2Fuploads%2F2009%2F03%2Fhdr-panoramic-landscape-final-175x175.jpg&f=1&nofb=1",
+      "https://antongorlin.com/wp-content/gallery/mountain-landscape/thumbs/thumbs_mountain-image-road.jpg",
+      "https://antongorlin.com/wp-content/gallery/landscape/thumbs/thumbs_dark-forest-background.jpg",
+   ];
+
    return (
       <div className={styles["server-bar"]}>
-         <ServerIcon icon={""} showPill={false} />
+         <ServerIcon icon={FaDiscord} colourVariant={ServerIconColourVariant.None} showPill={false} />
          <hr className={`${styles.divider} ${styles.center}`} />
          <div id="server-container">
             {servers.map((server, index) => (
-               <ServerIcon icon={""} showPill={true} index={index} onClick={() => dispatch(setSelectedServer(index))} key={index} />
-               // <input className={styles.server} type="image" src="http://icon-library.com/images/discord-transparent-server-icon/discord-transparent-server-icon-17.jpg"></input>//         
+               <ServerIcon
+                  icon={placeholderImages[index % placeholderImages.length]}
+                  showPill={true}
+                  index={index}
+                  onClick={() => dispatch(setSelectedServer(index))}
+                  key={index}
+               />
+               // <input className={styles.server} type="image" src="http://icon-library.com/images/discord-transparent-server-icon/discord-transparent-server-icon-17.jpg"></input>//
             ))}
          </div>
-         <ServerIcon showPill={false} icon={""} onClick={() => dispatch(addServer("new server"))} />
-         <ServerIcon showPill={false} icon={""} />
+         <ServerIcon
+            showPill={false}
+            icon={AiOutlinePlus}
+            colourVariant={ServerIconColourVariant.Green}
+            onClick={() => dispatch(addServer("new server"))}
+         />
+         <ServerIcon showPill={false} icon={IoMdCompass} colourVariant={ServerIconColourVariant.Green} />
          <hr className={`${styles.divider} ${styles.center}`} />
-         <ServerIcon showPill={false} icon={""} />
+         <ServerIcon showPill={false} icon={FiDownload} colourVariant={ServerIconColourVariant.Green} />
       </div>
-   )
+   );
 }
--- components/ServerIcon.tsx
+++ components/ServerIcon.tsx
@@ -1,27 +1,51 @@
 import { useEffect } from "react";
+import { IconType } from "react-icons";
 import { useSelector } from "react-redux";
 import { Store } from "../state/reducers";
-import styles from "../styles/ServerIcon.module.css"
+import styles from "../styles/ServerIcon.module.css";
+import blankIcon from "../public/blank48x48.png";
 
 interface ServerIconProps {
-   icon: string,
-   showPill: boolean,
+   icon: string | IconType;
+   colourVariant?: ServerIconColourVariant;
+   showPill: boolean;
    onClick?: () => void;
-   index?: number
+   index?: number;
+}
+
+export enum ServerIconColourVariant {
+   Green,
+   None,
 }
 
 export default function ServerIcon(props: ServerIconProps) {
-
    const selection = useSelector((state: Store) => state.selection);
 
    return (
       <div className={`${styles["server-icon"]} ${props.index === selection.server ? styles["selected"] : ""}`}>
-         <button  id="icon" className={styles.icon} onClick={props.onClick}></button>
-         {props.showPill ?
-         <div className={styles["pill-container"]}>
-             <div className={styles.pill}></div> 
-         </div>
-         : null }
+         {typeof props.icon === "string" ? (
+            <img
+               className={`${styles["icon-button"]}`}
+               src={typeof props.icon === "string" ? props.icon : ""}
+               onClick={props.onClick}
+            />
+         ) : (
+            <div
+               className={`${styles["icon-button"]} ${styles["icon-wrapper"]} ${
+                  props.colourVariant === ServerIconColourVariant.Green
+                     ? styles["icon-button-green"]
+                     : styles["icon-button-none"]
+               }`}
+               onClick={props.onClick}
+            >
+               <props.icon className={styles["icon"]} />
+            </div>
+         )}
+         {props.showPill ? (
+            <div className={styles["pill-container"]}>
+               <div className={styles.pill}></div>
+            </div>
+         ) : null}
       </div>
-   )
+   );
 }
